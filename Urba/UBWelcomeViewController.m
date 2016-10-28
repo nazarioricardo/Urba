f8cc3a2d80c0f4ad3d29c0872c497559a43d058a
Ricardo Nazario <nazarioricardo@gmail.com>
2016-10-28 14:21:09 -0400

    Created ActivityView class

--- Urba/UBWelcomeViewController.m
+++ Urba/UBWelcomeViewController.m
@@ -7,13 +7,18 @@
 //
 
 #import "UBWelcomeViewController.h"
+#import "ActivityView.h"
+
+NSString *const activityView = @"ActivityView";
 
 @import Firebase;
 
-@interface UBWelcomeViewController () 
+@interface UBWelcomeViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextField *emailTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextfield;
+
+//@property (weak, nonatomic) UBActivityViewController *activityView;
 
 @end
 
@@ -24,6 +29,7 @@
 
     [self logIn];
 }
+
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
     
@@ -39,6 +45,8 @@
 
 - (void)logIn {
     
+    ActivityView *spinner = [ActivityView loadSpinnerIntoView:self.view];
+    
     [[FIRAuth auth] signInWithEmail:@"nazarioricardo@gmail.com"
                            password:@"iamricky"
                          completion:^(FIRUser *user, NSError *error) {
@@ -46,13 +54,27 @@
                              if (error) {
                                  
                                  NSLog(@"There has been an error!\n %@", error.description);
-                             } else {
                                  
+                             } else {
+                                 [spinner removeSpinner];
                                  NSLog(@"Logged in as %@", user.email);
                                  [self performSegueWithIdentifier:@"LogInSegue" sender:self];
                              }
                          }];
 }
+
+//- (void)presentActivityView {
+//    
+//    self.activityView = [self.storyboard instantiateViewControllerWithIdentifier:activityView];
+//    
+//    // Change the size of page view controller
+//    self.activityView.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+//    
+//    [self addChildViewController:_activityView];
+//    [self.view addSubview:_activityView.view];
+//    [self.activityView didMoveToParentViewController:self];
+//    
+//}
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     [self.view endEditing:YES];
@@ -62,7 +84,7 @@
     
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    [self logIn];
+//    [self logIn];
 }
 
 - (void)didReceiveMemoryWarning {
