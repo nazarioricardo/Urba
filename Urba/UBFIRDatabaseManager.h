589c5ae4fdf86c4c3b6ef03a534f9005dd680762
Ricardo Nazario <nazarioricardo@gmail.com>
2016-11-01 11:04:18 -0400

    Working on FIRDBManager

---
+++ Urba/UBFIRDatabaseManager.h
@@ -0,0 +1,13 @@
+//
+//  UBFIRDatabaseManager.h
+//  
+//
+//  Created by Ricardo Nazario on 10/29/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UBFIRDatabaseManager : NSObject
+
+@end
---
+++ Urba/UBFIRDatabaseManager.m
@@ -0,0 +1,13 @@
+//
+//  UBFIRDatabaseManager.m
+//  
+//
+//  Created by Ricardo Nazario on 10/29/16.
+//
+//
+
+#import "UBFIRDatabaseManager.h"
+
+@implementation UBFIRDatabaseManager
+
+@end
