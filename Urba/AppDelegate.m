b70df4eb6267a6348d0c65a3a9b594646ce844ee
Ricardo Nazario <nazarioricardo@gmail.com>
2016-11-16 14:27:23 -0400

    Prepared Client for messaging

--- Urba/AppDelegate.m
+++ Urba/AppDelegate.m
@@ -37,6 +37,36 @@
     
 }
 
+- (void)tokenRefreshNotification:(NSNotification *)notification {
+    // Note that this callback will be fired everytime a new token is generated, including the first
+    // time. So if you need to retrieve the token as soon as it is available this is where that
+    // should be done.
+    NSString *refreshedToken = [[FIRInstanceID instanceID] token];
+    NSLog(@"InstanceID token: %@", refreshedToken);
+    
+    // Connect to FCM since connection may have failed when attempted before having a token.
+    [self connectToFcm];
+    
+    // TODO: If necessary send token to application server.
+}
+
+- (void)connectToFcm {
+    [[FIRMessaging messaging] connectWithCompletion:^(NSError * _Nullable error) {
+        if (error != nil) {
+            NSLog(@"Unable to connect to FCM. %@", error);
+        } else {
+            NSLog(@"Connected to FCM.");
+        }
+    }];
+}
+
+// With "FirebaseAppDelegateProxyEnabled": NO
+- (void)application:(UIApplication *)application
+didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    [[FIRInstanceID instanceID] setAPNSToken:deviceToken
+                                        type:FIRInstanceIDAPNSTokenTypeSandbox];
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
